1b3e255eed9ef33c135be02e2b58bfd4b71f4240
cerber-os <cerber@cerberos.pl>
2019-03-06 19:04:37 +0100

    Initial commit

---
+++ Project1/main.cpp
@@ -0,0 +1,116 @@
+/*
+ * File: main.cpp
+ * Author: Paweł Wieczorek
+ * Date: 04 mar 2019
+ */
+
+#include <iostream>
+#include "realSet.h"
+
+#define NUMBER_OF_SETS      10
+
+using namespace std;
+
+enum menuOptions{M_APPEND = 1, M_REMOVE, M_LENGTH, M_PRINT, M_SUM, M_DIFF, M_INTER, M_EQ, M_EXIT};
+
+void printHelp() {
+    cout << "############ Usage ##########" << endl;
+    cout << "1. Append element to set" << endl;
+    cout << "2. Remove element from set" << endl;
+    cout << "3. Get number of elements in set" << endl;
+    cout << "4. Print set" << endl;
+    cout << "5. Sum of sets" << endl;
+    cout << "6. Sets difference" << endl;
+    cout << "7. Sets intersection" << endl;
+    cout << "8. Check equality" << endl;
+    cout << "9. Exit" << endl;
+    cout << "###########################" << endl;
+}
+
+int getIdx() {
+    int idx;
+
+    cout << "Id of set: ";
+    cin >> idx;
+    if( idx < 0 || idx >= NUMBER_OF_SETS ) {
+        cerr << "Err: Invalid index! Must be integer between 0 and " << NUMBER_OF_SETS - 1 << ". Abort" << endl;
+        abort();
+    }
+    return idx;
+}
+
+int getValue() {
+    int value;
+    cout << "Value: ";
+    cin >> value;
+    if(!cin) {
+        cerr << "Err: Invalid input! Expected integer... Abort" << endl;
+        abort();
+    }
+    return value;
+}
+
+int main() {
+    int choice = 0;
+    int opt1, opt2;
+    RealSet sets[NUMBER_OF_SETS];
+
+    while(choice != M_EXIT)
+    {
+        printHelp();
+        cin >> choice;
+        switch(choice) {
+            case M_APPEND:
+                opt1 = getIdx();
+                sets[opt1].append(getValue());
+                cout << "Done!" << endl;
+                break;
+            case M_REMOVE:
+                opt1 = getIdx();
+                sets[opt1].remove(getValue());
+                cout << "Done!" << endl;
+                break;
+            case M_LENGTH:
+                opt1 = getIdx();
+                cout << "Length of #" << opt1 << ": " << sets[opt1].getLength() << endl;
+                break;
+            case M_PRINT:
+                opt1 = getIdx();
+                cout << "Set #" << opt1 << ": " << sets[opt1] << endl;
+                break;
+            case M_SUM:
+                opt1 = getIdx();
+                opt2 = getIdx();
+                sets[opt1] += sets[opt2];
+                cout << "Done! Result stored in #" << opt1 << endl;
+                break;
+            case M_DIFF:
+                opt1 = getIdx();
+                opt2 = getIdx();
+                sets[opt1] -= sets[opt2];
+                cout << "Done! Result stored in #" << opt1 << endl;
+                break;
+            case M_INTER:
+                opt1 = getIdx();
+                opt2 = getIdx();
+                sets[opt1] *= sets[opt2];
+                cout << "Done! Result stored in #" << opt1 << endl;
+                break;
+            case M_EQ:
+                opt1 = getIdx();
+                opt2 = getIdx();
+                if(sets[opt1] == sets[opt2])
+                    cout << "Sets #" << opt1 << " and #" << opt2 << " are IDENTICAL" << endl;
+                else
+                    cout << "Sets #" << opt1 << " and #" << opt2 << " are DIFFERENT" << endl;
+                break;
+            case M_EXIT:
+                break;
+            default:
+                cerr << "Err: Unknown option!" << endl;
+                cin.clear();
+                cin.ignore(INT16_MAX, '\n');    // Remove invalid character from stdin
+        }
+    }
+    return 0;
+}
---
+++ Project1/realSet.cpp
@@ -0,0 +1,207 @@
+/*
+ * File: realSet.cpp
+ * Author: Paweł Wieczorek
+ * Date: 04 mar 2019
+ * Description: Type used to store set of real numbers
+ */
+
+#include "realSet.h"
+#include <iostream>
+
+
+RealSet::RealSet() {
+    set = new double[MAX_SIZE + 1];
+    length = 0;
+}
+
+RealSet::~RealSet()
+{
+    delete[] set;
+}
+
+/**
+ * Checks if set contains given number
+ * @param number
+ * @return true | false
+ */
+bool RealSet::contains(double number) const {
+    for(int i = 0; i < length; i++)
+        if(set[i] == number)
+            return true;
+    return false;
+}
+
+/**
+ * Adds number to set
+ * @param number
+ */
+void RealSet::append(double number) {
+    if(contains(number))
+        return;
+
+    if(length == MAX_SIZE)
+        std::cerr << "Err: Max set size reached. Ignoring append request..." << std::endl;
+    else
+        set[length++] = number;
+}
+
+/**
+ * Removes given number from set
+ * @param number
+ * @return removed number or -1 if it isn't in set
+ */
+double RealSet::remove(double number) {
+    int indexOfNumber = -1;
+    for (int i = 0; i < length; i++)
+        if (set[i] == number) {
+            indexOfNumber = i;
+            break;
+        }
+
+    if (indexOfNumber == -1) {
+        // There's no such number in set
+        std::cerr << "Err: Attempted to remove not existing element. Returning -1..." << std::endl;
+        return 0;
+    } else if(indexOfNumber == length - 1)
+        // It's the last value in set
+        return set[length--];
+
+    // It's typical case - insert last element in the place of removed one
+    double retVal = set[indexOfNumber];
+    set[indexOfNumber] = set[length - 1];
+    length--;
+    return retVal;
+}
+
+/**
+ * Returns number of elements in set
+ * @return length
+ */
+int RealSet::getLength() const {
+    return length;
+}
+
+/**
+ * Gets i-th element from set. Be careful, index of number could change after modification of set
+ * @param index of element
+ * @return value or -1 in case such index does not exist
+ */
+double RealSet::getIthElement(int index) const {
+    if(index < 0 || index >= length) {
+        std::cerr << "Err: Attempted to get not existing element. Returning -1..." << std::endl;
+        return -1;
+    }
+    return set[index];
+}
+
+/**
+ * Sum of sets
+ * @param oper
+ * @return
+ */
+RealSet RealSet::operator+(const RealSet& oper) {
+    RealSet newSet;
+
+    for(int i = 0; i < getLength(); i++)
+        newSet.append( getIthElement(i) );
+    for(int i = 0; i < oper.getLength(); i++)
+        if(!newSet.contains( oper.getIthElement(i) ))
+            newSet.append(oper.getIthElement(i));
+    return newSet;
+}
+
+void RealSet::operator+=(const RealSet& oper) {
+    for(int i = 0; i < oper.getLength(); i++)
+        if(!contains( oper.getIthElement(i) ))
+            append(oper.getIthElement(i));
+}
+
+/**
+ * Sets difference
+ * @param oper
+ * @return
+ */
+RealSet RealSet::operator-(const RealSet& oper) {
+    RealSet newSet;
+
+    for(int i = 0; i < getLength(); i++)
+        if(!oper.contains( getIthElement(i) ))
+            newSet.append(getIthElement(i));
+    for(int i = 0; i < oper.getLength(); i++)
+        if(!contains( oper.getIthElement(i) ))
+            newSet.append(oper.getIthElement(i));
+    return newSet;
+}
+
+void RealSet::operator-=(const RealSet& oper) {
+
+    for(int i = 0; i < oper.getLength(); i++)
+        if(contains( oper.getIthElement(i) ))
+            remove(oper.getIthElement(i));
+        else
+            append(oper.getIthElement(i));
+}
+
+/**
+ * Sets intersection
+ * @param oper
+ * @return
+ */
+RealSet RealSet::operator*(const RealSet& oper) {
+    RealSet newSet;
+    for(int i = 0; i < getLength(); i++)
+        if(oper.contains(getIthElement(i)))
+            newSet.append(getIthElement(i));
+    return newSet;
+}
+
+void RealSet::operator*=(const RealSet& oper) {
+    for(int i = 0; i < getLength(); i++)
+        if(!oper.contains(getIthElement(i)))
+            remove(getIthElement(i--));     // We have removed element, but on its place the last one was placed
+                                            // so by decreasing i we are checking also the new one
+}
+
+/**
+ * Checks if two sets contains the same numbers
+ * @param oper
+ * @return true | false
+ */
+bool RealSet::operator==(const RealSet& oper) {
+    if(getLength() != oper.getLength())
+        return false;
+    for(int i = 0; i < getLength(); i++)
+        if(!oper.contains(getIthElement(i)))
+            return false;
+    return true;
+}
+
+bool RealSet::operator!=(const RealSet& oper) {
+    if(getLength() != oper.getLength())
+        return true;
+    for(int i = 0; i < getLength(); i++)
+        if(!oper.contains(getIthElement(i)))
+            return true;
+    return false;
+}
+
+/**
+ * Returns max possible set sized. This can be changed by editing const value MAX_SIZE
+ * @return max size of set
+ */
+int RealSet::getMaxSize() const {
+    return MAX_SIZE;
+}
+
+/**
+ * Inserts text representation of set to stream
+ * @param out
+ * @param oper
+ * @return
+ */
+std::ostream& operator<<(std::ostream& out, const RealSet& oper) {
+    for(int i = 0; i < oper.getLength() - 1; i++)
+        out << oper.getIthElement(i) << ", ";
+    out << oper.getIthElement(oper.getLength() - 1); // Don't print comma at the end of output
+    return out;
+}
